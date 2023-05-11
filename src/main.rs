2423a4e08a2d091aee23386331b163cd5e1e2486
MathxH Chen <brainfvck@foxmail.com>
2023-05-11 16:14:53 +0800

    Add NN backbone

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+
+/// neural network  struct definition
+#[derive(Debug)]
+struct NeuralNetwork {
+    // input: Vec<f64>,
+    // hidden: Vec<f64>,
+    // output: Vec<f64>,
+    // weights_ih: Vec<f64>,
+    // weights_ho: Vec<f64>,
+    // bias_h: Vec<f64>,
+    // bias_o: Vec<f64>,
+    // learning_rate: f64,
+}
+
+impl NeuralNetwork {
+    fn new() -> NeuralNetwork {
+        NeuralNetwork {
+            // input: Vec::new(),
+            // hidden: Vec::new(),
+            // output: Vec::new(),
+            // weights_ih: Vec::new(),
+            // weights_ho: Vec::new(),
+            // bias_h: Vec::new(),
+            // bias_o: Vec::new(),
+            // learning_rate: 0.0,
+        }
+    }
+
+    fn train(&self) {
+        println!("train");
+    }
+
+    fn predict(&self) {
+        println!("predict");
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
