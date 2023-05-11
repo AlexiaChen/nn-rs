1ac99e8215dd03573320af0dff6b06acb67bbcd9
MathxH Chen <brainfvck@foxmail.com>
2023-05-11 16:24:39 +0800

    update comments

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,20 @@ struct NeuralNetwork {
     hidden_nodes: i32,
     output_nodes: i32,
     learning_rate: f64,
+    wih: Vec<f64>, // weights from input to hidden layer
+    who: Vec<f64>, // weights from hidden to output layer
 }
 
 impl NeuralNetwork {
     /// Create a new neural network from inputnodes, hiddennodes, outputnodes, learningrate
     fn new(inputnodes: i32, hiddennodes: i32, outputnodes: i32, learningrate: f64) -> NeuralNetwork {
+        
+        // link weight matrices, wih and who
+        // weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        // w11 w21
+        // w12 w22 etc 
+
+        
         NeuralNetwork {
             // set number of nodes in each input, hidden, output layer
             input_nodes: inputnodes,
