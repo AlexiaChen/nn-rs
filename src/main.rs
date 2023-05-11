20be298038509642de4b8fbd23c81a430b3b5660
MathxH Chen <brainfvck@foxmail.com>
2023-05-11 21:25:43 +0800

    Refactor test in main fn

--- src/main.rs
+++ src/main.rs
@@ -84,10 +84,12 @@ impl NeuralNetwork {
 fn main() {
     let nn = NeuralNetwork::new(3, 3, 3, 0.3);
     println!("NN is: {:?}", nn);
-    nn.train();
     let input_list = vec![1.0, 0.5, -1.5];
+    let target_list = vec![0.5, 1.0, 0.5];
     let o = nn.predict(&input_list);
-    // print 2D array from ndarray
-    println!("Output Vector is: {:?}", o);
+    println!("Output Vector Before train is: {:?}", o);
+    nn.train(&input_list, &target_list);
+    let o = nn.predict(&input_list);
+    println!("Output Vector After train is: {:?}", o);
    
 }
