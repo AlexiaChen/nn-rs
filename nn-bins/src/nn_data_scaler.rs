633efc3df50b2072bda95a9d7ccd6e58a440a349
MathxH Chen <brainfvck@foxmail.com>
2023-05-18 14:40:09 +0800

    Add nn scaler for train data

--- nn-bins/src/nn_data_scaler.rs
+++ nn-bins/src/nn_data_scaler.rs
@@ -1,9 +1,9 @@
 use ndarray::{Array, Dim};
-use image::{ImageBuffer, GrayImage};
 use std::fs::File;
 use std::io::prelude::*;
 use csv::{ReaderBuilder, StringRecord};
 use std::error::Error;
+
 
 fn get_img_pixels_data(record: &StringRecord) -> Vec<u8> {
     let mut image_vec: Vec<u8> = vec![];
@@ -17,23 +17,6 @@ fn get_img_pixels_data(record: &StringRecord) -> Vec<u8> {
         }
     }
     image_vec
-}
-
-fn save_record_to_image(record: &StringRecord, file_name: &str) {
-    // read digital pixels and convert to ndarray image
-    let image_vec: Vec<u8> = get_img_pixels_data(record);
-
-    let image_array: Array<u8, Dim<[usize; 2]>> = Array::from_shape_vec((28, 28), image_vec).unwrap();
-    let image_buffer = ImageBuffer::from_fn(28, 28, |x, y| {
-        // use [[y, x]] instead of [[x, y]] to access the pixel value at (x, y) in the pixels array is 
-        // because ndarray uses row-major order to store its elements.
-        let gray_value = image_array[[y as usize, x as usize]];
-        GrayImage::from_pixel(1, 1, image::Luma([gray_value]))
-            .get_pixel(0, 0)
-            .to_owned()
-    });
-
-    image_buffer.save(file_name).unwrap();
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -60,10 +43,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("Records len: {:?}", records.len());
     
-    save_record_to_image(&records[0], "./images/five.png");
-    save_record_to_image(&records[1], "./images/zero.png");
-    save_record_to_image(&records[2], "./images/four.png");
+    let record_0 = &records[0];
+    let image_vec: Vec<u8> = get_img_pixels_data(record_0);
+    let image_vec: Vec<f32> = image_vec.iter().map(|x| *x as f32).collect();
 
+    
+    let image_array: Array<f32, Dim<[usize; 2]>> = Array::from_shape_vec((28, 28), image_vec).unwrap();
+
+    let  scaled_data = image_array / 255.0 * 0.99 + 0.01;
+    println!("Scaled data: {:?}", scaled_data);
 
     Ok(())
    
